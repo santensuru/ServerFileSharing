7d96ae0d8d8f8a3cac2713c5662970fbea55882f
santensuru <djuned.ong@gmail.com>
2014-11-04 21:37:02 +0700

    Initial Commit > Server read file and send to client :)

---
+++ src/tugas05_server/Tugas05_server.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas05_server;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ *
+ * @author user
+ */
+public class Tugas05_server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        ServerSocket servsock = new ServerSocket(6060);
+        File myFile = new File("s.pdf");
+        while (true) {
+            Socket sock = servsock.accept();
+            byte[] mybytearray = new byte[(int) myFile.length()];
+            
+            Path file = Paths.get("s.pdf");
+            BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+
+            System.out.println("creationTime: " + attr.creationTime());
+            System.out.println("lastAccessTime: " + attr.lastAccessTime());
+            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+
+            System.out.println("isDirectory: " + attr.isDirectory());
+            System.out.println("isOther: " + attr.isOther());
+            System.out.println("isRegularFile: " + attr.isRegularFile());
+            System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
+            System.out.println("size: " + attr.size());
+            
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(myFile));
+            bis.read(mybytearray, 0, mybytearray.length);
+            OutputStream os = sock.getOutputStream();
+            os.write(mybytearray, 0, mybytearray.length);
+            os.flush();
+            sock.close();
+        }
+    }
+    
+}
